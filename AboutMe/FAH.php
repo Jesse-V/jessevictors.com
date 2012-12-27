2bfc1c605258c9fc21e0ad28e8f617eeefa82609
JesseVictors <jvictors@jessevictors.com>
2012-12-27 00:01:48 -0900

    wrote F@h page, testing results from different browsers

--- AboutMe/FAH.php
+++ AboutMe/FAH.php
@@ -5,11 +5,40 @@
 
 			<div id="content">
 				<p>
-					I also have a great deal of interest in a scientific research project called Folding@home, which I joined in October 2010. This project revolves around thousands of people around the world running a small program on their computer, which does calculations for Stanford University to accelerate their disease research. Like most everyone participating in this project, I don’t know need to know any technical biochemistry knowledge, but all I have to do is run their program on my computer. It performs simulations of proteins and other molecules changing shape in the body, because if things go wrong there then horrible diseases like Alzheimer’s, cancer, or Parkinson’s may occur. It doesn’t even interfere with my regular computer usage, and its nice knowing that I can power state-of-the-art research without having to spend any money. Scientists have produced more than 95 scientific papers as a result of Folding@home with much of the promising results open to the public, and it is arguably the most powerful computing system on the planet today. I’ve led the “USU Aggies” team high into the rankings, and overall we’ve contributed an astonishing amount of computing to the project. I really feel that our contributions have significantly helped Folding@home, and I know they are being put to good use.
-					
-					As my interest in the project grew, so did the level of my involvement. Starting from a pretty dismal state, I wrote nearly the entirety of the 6,900-word Folding@home article on Wikipedia and achieved its Good Article status in April 2012, which meant that other editors recognized it as pretty encyclopedic and well-written, although further tweaks could still be made before it could be considered top-notch. The article is read between 400 and 500 times per day. I’m active on the Folding@home support forum, and my feedback has even been quoted by the director of the project, Dr. Pande from Stanford University, in one of his blog posts. When a scientists begins studying an interesting protein, I’m one of several dozen volunteers who test the stability of newly-minted simulation units when they are first released outside of the research lab. Aside from actually working at Stanford itself, I consider this to be as close to the front lines of disease research as I could get. I’ve also been granted permission to edit the FAQ pages of the project’s official website, which is typically exclusively edited by mods, admins, and research scientists.
-					
-					Alzheimer’s, cancer, Creutzfeldt–Jakob disease, Huntington's disease, and viral infection have killed millions in the past. Cancer in particular can devastate and kill a young child before they’ve had a chance to fully enjoy life. I know of no other project where anyone can help power state-of-the-art research towards the root causes of these diseases without donating any money, while being kept up-to-date of where the efforts are going and the progress the project is making towards cures.
+					I also have a great deal of interest in a scientific research project called Folding@home, which I joined in October 2010. The project is run out of Stanford University and primarily focuses on disease research. I think this project is rather unique because it relies on laymen volunteers all around the world who all help accelerate the research.  The scientists behind Folding@home are interested in understanding how specific molecules (proteins, phospholipid membrane, etc) move and interact in the body, and when something happens and things go wrong, what exactly leads to big diseases like cancer and Alzheimer's.
+				</p>
+				<p>
+					One of these interesting molecules is a special protein (a sequence of amino acids, all linked together) called P53. This protein is essential to keeping a cell growing properly. In the event of damage to a cell's DNA, P53 keeps the cell from growing any further and it signals other molecules to try to repair the DNA. In effect, P53 wards off cancer. Before it can do this job, it first needs to take on a particular and unique shape -- a shape key to its role. The problem is, sometimes P53 "folds" into something completely different, so it ends up in the wrong shape. Then it can't do its job, and cancer can result. Folding@home scientists are interested in understanding what chemical forces drive P53 to a misshapen state, and what they can do to correct it before or after it gets there. There are other proteins and different molecular processes behind other diseases, like Alzheimer's, Huntington's, Creutzfeldt–Jakob disease, and Type II diabetes.
+				</p>
+				<p>
+					Most proteins assemble (fold) themselves into a particular shape in a thousandth of a second, some as fast as a millionth! It's a real challenge to study such a high-speed process, and physical experiments and tests tell scientists some information, but not enough. They need to be able to see the big picture, as well as many of the missing details. For this they turn to simulations. The problem is, it takes an extremely long time for a computer to complete the simulations: there is just so much going on. It would take a regular computer over thirty years to finish one protein! Supercomputers are really expensive to build and operate, so clearly medical researchers have a problem.
+				</p>
+				<p>
+					This is where we come in. I'm not a biochemist, and I'm certainly not an expert in computational molecular biology, but all I have to do is run a simple program on my computer. It runs behind the scenes, and I rarely need to mess with it. My home computer joins hundreds of thousands of others across the world, we're all folding proteins, and each one of us is running a piece of the simulation. United like this, Folding@home is one of the most powerful computational forces on the planet, and for a good number of years was three to four times faster than the best supercomputer. Since it's launch in 2000, over one hundred scientific research papers have been written as a direct result of the project. The project's lead scientist and director, Dr. Vijay Pande, has co-authored all these papers and his works have been referenced over 11,400 times by other publications, so Folding@home has clearly made a big impact in the scientific community. They've also made excellent progress looking for drug candidates, so the project has certainly been productive.
+				</p>
+				<p>
+					Since October 2010, I have become more and more involved in Folding@home. The project is just so fascinating! Aside from actually working at the lab at Stanford, I've helped out in nearly every aspect of Folding@home.
+					<ul>
+						<li>
+							I founded the USU Aggies team, and I continue to participate and lead us high into the rankings.
+						</li>
+						<li>
+							In early August 2011 I started cleaning up and expanding in Wikipedia article on Folding@home, and I finally completed all of my work on the article in October 2012 when it became a Featured Article. This meant that it was officially recognized as complete, well-written, and one of the highest-quality and encyclopedic articles Wikipedia offers. It appeared as Wikipedia's Featured Article of the Day on November 1st, 2012, which was followed by a couple of posts on the official Folding@home blog. The article is read between 400 and 500 times per day. It's fantastic that my writing efforts benefit so many people!
+						</li>
+						<li>
+							Since July 2011, I've been very active on the Folding@home support forum answering question and discussing F@h with others including the main scientists.
+						</li>
+						<li>
+							I'm one of several dozen volunteer that test newly-minted simulation units when they are first released outside of the research lab. If something goes wrong with them, we report the problems. This quality assurance testing helps keep unstable units from getting out to everyone else.
+						</li>
+						<li>
+							Dr. Pande granted me permission to edit and update the official F@h website. There are only a few other maintainers of this site, including Tim "7im" Braun, Bruce Borden (the main admin on the F@h forum), PantherX (a mod on the forum), and the main software developer, Joseph Coffland.
+						</li>
+						<li>
+							I help out the software developers, and one of my tasks is to concisely report bugs in the software. Only a dozen others do this. I've participated in discussions about new F@h features, and reported several serious problems with early versions of the software. For example <a href="https://fah-web.stanford.edu/projects/FAHClient/ticket/958">this</a> is one of the issues I reported.
+						</li>
+					</ul>
+					It is so neat to be so near the front lines of medical research. I enjoy being kept up-to-date with where my efforts are going and the progress they're making towards cures. I don't know any way that I can accelerate research this much!
 				</p>
 			</div>
 
