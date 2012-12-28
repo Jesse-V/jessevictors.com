db817ba4aa727f148abc279a437f2078d23ea7e1
JesseVictors <jvictors@jessevictors.com>
2012-12-27 19:32:18 -0900

    text for Wikipedia page, will add explanatory links

--- AboutMe/Wikipedia.php
+++ AboutMe/Wikipedia.php
@@ -4,7 +4,79 @@
 ?>
 
 			<div id="content">
-				
+				<p>
+					Sometimes in my free time I log on to Wikipedia. Wikipedia is Massively Multiplayer Online Role-Playing Game (MMORPG), and has an immersive gameworld with over 18.1 million players and over 4.13 million unique locations, including 174,000 undiscovered secret areas, 16400 completely explored dungeons and 3800 boss levels. Magical items and powers can be found scattered over these locations, whose navigation is aided by maps of the game world. Some of these locations, as well as items and powers, become problematic, and unbalance the game world. Players will generally dislike these, and Game Masters respond by destroying them.
+				</p>
+				<p>
+					Every unique location in the gameworld has a tavern where players have the opportunity to interact with any other player in real time. Players often become friends with other players, and some have even arranged to meet in real life. Players may even build their own houses and receive messages from other players. On the darker side, some players become addicted, unable to leave the game, and spend all their waking hours on the site. It is often the most experienced players who end up spending the most amount of time in the gameworld.
+				</p>
+				<p>
+					Players can accumulate EXP (experience points), allowing them to advance to higher levels. Players develop a unique distribution of stats, and can choose between a number of classes, such as Gunner, Trickster, Arch Mage, Sorcerer, Archer, and Priest, as well as choosing from one of several races, such as gnomes, ogres and imps. In some cases, a player may choose to play with excessive summons, or play with multiple warriors at the same time. This provides an additional challenge, because they must avoid being caught by Bounty Hunters, who are tasked with rooting them out and destroying them. The in-game currency (WikiMoney) has largely fallen out of use, and most players prefer to accumulate different types of stars and various icons which other players can award them. Players can take on quests, fight boss battles, enter battle arenas, and even take class change trials to become Game Masters. It is possible to stumble upon random encounters and defeat such monsters.
+				</p>
+				<p>
+					Trolls are one of the most common enemies in the gameworld, and often turn up in boss fights. Orcs, the natural enemies of the Trolls, who oppose trolls using methods which are equally unethical or destructive (such as baiting) can also be fought. In combat, weapons such as guns are used normally, but cannons and swords can be used to defeat an undefeatable enemy. Many players also carry a shield to protect themselves. Often, players will monitor dungeons that they are (or have been) involved with with special cameras, which are connected to a private area which belongs to that player and that only he or she can access, and then only when logged in, so that enemies cannot use it to their advantage. 
+				</p>
+				<p>
+					Guns can be used in places other than battle arenas for clearing the problems of a dungeon, but excessive use of guns will probably be censured by the Imperial Court of Justice. Barbarian-fighting is a form of combat that has many similarities to a variation of spawn kill. Illegitimate battle strategies are abundant. Sneaky players like to use excessive summons and minions to trick Game Masters.
+				</p>
+				<p>
+					Players can also attempt to destroy, mangle or disrupt parts of the game world. Player-killing is strongly discouraged, but nevertheless happens. New players are most vulnerable. Game Masters attempt to control this by placing offending players in ice blocks, freeze locks, or bans, but more often than not, criminals end up being brought before the Super Game Masters, who have the final say in dealing with them.
+				</p>
+				<p>
+					Some Wikipedia players who successfully complete a class change trial manage to become Game Masters, a secretive clan whose members have access to additional spells and items such as:
+				</p>
+				<p>
+					<ul>
+						<li>
+							Cave-in – a powerful spell which causes a dungeon's walls to collapse, making it inaccessible to all but the most experienced adventurers (game masters and bounty hunters).
+						</li>
+						<li>
+							<ul>
+								<li>
+									Mining Toolkit – a number of mining tools such as a mining helmet and wooden support beams which enable Game Masters to explore collapsed dungeons and re-open them to others if they want.
+								</li>
+								<li>
+									Nuke - explodes to collapse all dungeons of a player.
+								</li>
+							</ul>
+						</li>
+						<li>
+							Ice Block – encases another player or minion in a block of ice, preventing them from moving for a certain length of time (or forever!). A similar version is the Freeze Lock, which locks gates or entire borders of gates from entry. Whether the frozen player can speak or use hand gestures or not is up to the game master in either case. Any gate next to a player in an ice block automatically freeze locks.
+						</li>
+						<li>
+							Thaw – thaws a frozen player or minion out. A similar item is the Sandwich-warming glove, which is a wearable item that allows Game Masters and players permitted by Game Masters to enter through any gate, whether locked or not.
+						</li>
+						<li>
+							Magic Chains – seals the entrance to a dungeon with magical chains which can only be bypassed with special items. Some dungeons' magic chains are absolutely permanent and can never be removed, although the special items still allow a player carrying them to bypass them.
+						</li>
+						<li>
+							Skeleton Key – a magical key given to all Game Masters which can remove any magical chains around a dungeon entrance for that player.
+						</li>
+						<li>
+							Cloak of Silence – quietens a player's footsteps, enabling them to avoid patrols. A more powerful version of the Boots of Stealth, which can be worn by most players.
+						</li>
+							Rapid-Fire – can massively subdue multiple attacks at once. A more powerful version of Single-Fire, which can be used by most players using guns. Rapid-Fire can be utilized by normal players by becoming Gunners, but only with the permission of Game Masters.
+						</li>
+						<li>
+							Teleport – can create an inter-dimensional portal that can summon a large number of dungeons.
+						</li>
+						<li>
+							Brick dissolver: Only game masters may use a brick dissolver, which allows bricks in a dungeon wall to be dissolved. A dissolved brick is nothing but a clear space that still supports the bricks above it, and can be only viewed by game masters or miners. Whether the signature (edit summary) or even the name of the player who made the brick gets dissolved or not can be decided by the game master. The solvent is 69M per-unobtanic acid.
+						</li>
+						<li>
+							Brick crystallizer: A game master can also choose to re-crystallize a brick that has been dissolved, turning it back into a normally-functional brick.
+						</li>
+					<ul>
+				</p>
+				<p>
+					Wikipedia players can be promoted to Game Master (or demoted!) by even more powerful users known as Overlords, who, in addition to having the power to appoint new Game Masters and Overlords, can summon many powerful minions to aid them in their adventuring.
+				</p>
+				<p>
+					The Dark Lord
+					</br>
+					The master and founder of Wikipedia and its related worlds. He patrols the game world, and in former times he has had complete and total authority, and still is the only one besides the Knights who is allowed to change any user's user classes (including Knights). However, his power has been nerfed over recent years. Although he has become reluctant to use these powers, he still has methods of overriding the actions of Game Masters, and even Super Game Masters, by more subtle means. If a player feels that the game has treated them unfairly, they can create free amusement for other players by asking the Dark Lord for assistance.
+				</p>
+				:)	
 			</div>
 
 <?php
