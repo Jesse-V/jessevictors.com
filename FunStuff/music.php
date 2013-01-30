59b34a14974c374230420cc5d562e3582855ac09
JesseVictors <jvictors@jessevictors.com>
2013-01-29 20:34:15 -0700

    expanded music and video pages

--- FunStuff/music.php
+++ FunStuff/music.php
@@ -4,7 +4,49 @@
 ?>
 
 			<div id="content">
-				I mainly listen to only true music. There are only a small number of people in this world who can play true music, but those that do expertly wield a small number of instruments.  These artists include Howard Shore, (who composed and produced the soundtrack to The Lord of the Rings) as well as the PianoGuys, and Lindsey Stirling, who are both on YouTube.
+				<p>
+					I have pretty high standards when it comes to music. Over the last couple of decades I think music quality has decline, and most artists have lost the sense of "true music". Fortunately, a few remain who are extremely skilled at their instruments. These include Howard Shore (the producer of the Lord of the Rings soundtrack), the Piano Guys, and Lindsey Stirling. Here are some examples of excellent music videos:
+				</p>
+
+				<div class="youtube">
+					<div class="label">Michael Meets Mozart, Piano Guys</div>
+					<iframe class="video" src="http://www.youtube.com/embed/rR94NDIfGmA?rel=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+
+				<div class="youtube">
+					<div class="label">All Of Me, Piano Guys</div>
+					<iframe class="video" src="http://www.youtube.com/embed/9fAZIQ-vpdw?rel=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+
+				<div class="youtube">
+					<div class="label">Lord of the Rings Medley, Piano Guys</div>
+					<iframe class="video" src="http://www.youtube.com/embed/FZNqs0YgWkM?rel=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+
+				<div class="youtube">
+					<div class="label">Crystallize, Lindsey Stirling</div>
+					<iframe class="video" src="http://www.youtube.com/embed/aHjpOzsQ9YI?rel=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+
+				<div class="youtube">
+					<div class="label">Spontaneous Me, Lindsey Stirling</div>
+					<iframe class="video" src="http://www.youtube.com/embed/pRPOztxXWlQ?rel=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+
+				<div class="youtube">
+					<div class="label">Gondor/Rohan Themes, Howard Shore</div>
+					<iframe class="video" src="http://www.youtube.com/embed/_dypVv0NouI?rel=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+
+				<div class="youtube">
+					<div class="label">Danger Zone</div>
+					<iframe class="video" src="http://www.youtube.com/embed/V8rZWw9HE7o?rel=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+
+				<p>
+					<br>
+					I've compiled a playlist of some of my favorite music videos, including these. See <a href="http://www.youtube.com/playlist?list=PLGXAmsf4iPiFJbFZtVdfbYLoPNiZyww4L">the playlist on my YouTube channel</a>.
+				</p>
 			</div>
 
 <?php
--- FunStuff/videos.php
+++ FunStuff/videos.php
@@ -4,7 +4,10 @@
 ?>
 
 			<div id="content">
-				
+				<iframe class="youtube" src="http://www.youtube.com/embed/xY_MUB8adEQ?rel=0" frameborder="0" allowfullscreen></iframe>
+				<p>
+					More coming soon. :)
+				</p>
 			</div>
 
 <?php
