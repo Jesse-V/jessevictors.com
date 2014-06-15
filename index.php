891fac10aa22ba634e453d2a62cd057fa4408e6b
Jesse_V <jvictors@jessevictors.com>
2014-06-15 10:47:12 -0800

    reorganized homepage, better styling

--- index.php
+++ index.php
@@ -4,11 +4,9 @@
     require_once('common/header.php');
 ?>
 
-   <div id="title">
-      JesseVictors.com
-   </div>
-
    <div class="rightSide">
+      <p><b>This news bar is a work-in-progress!</b></p>
+
       <div class="news section">
          <div class="subtitle">Latest News</div>
          <div class="item">News piece 1</div>
@@ -25,7 +23,7 @@
          <div class="commit">pushed "layout fixes" to Github</div>
       </div>
       <div class="torStatus section">
-         <div class="subtitle">Tor Relay Status</div>
+         <div class="subtitle">Tor Status</div>
          <table>
             <tr>
                <th>Relay</th>
@@ -82,11 +80,36 @@
                <td>54.04 kB/sec</td>
             </tr>
          </table>
+         <table>
+            <tr>
+               <td>Tor users today</td>
+               <td>2,459,127</td>
+            </tr>
+            <tr>
+               <td>Relays</td>
+               <td>5,242</td>
+            </tr>
+            <tr>
+               <td>Bridges</td>
+               <td>3,212</td>
+            </tr>
+         </table>
+         <div class="news">
+            <div class="item">06/11: <a href="https://blog.torproject.org/blog/tor-weekly-news-%E2%80%94-june-11th-2014">Tor Weekly News - June 11th, 2014</a></div>
+            <div class="item">06/10: <a href="https://blog.torproject.org/blog/tails-101-out">Tails 1.0.1 is out</a></div>
+            <div class="item">06/09: <a href="https://blog.torproject.org/blog/tor-browser-362-released">Tor Browser 3.6.2 is released</a></div>
+         </div>
       </div>
-      <div class="miscStatus section">
+      <div class="fah section">
          <div class="subtitle">Folding@home Productivity</div>
          <div class="status">24,523 points/day</div>
-
+         <div class="news">
+            <div class="item">06/02: Webinar tomorrow - a chance for Q&A</div>
+            <div class="item">05/29: NaCl client points change</div>
+            <div class="item">05/23: Webinar June 3, 2014: Next Steps...</div>
+         </div>
+      </div>
+      <div class="crypto section">
          <div class="subtitle">Cryptocurrency</div>
          <div class="coin">
             <div class="title">Bitcoin (...77g)</div>
@@ -132,89 +155,94 @@
       </div>
    </div>
 
-   <div class="leftSize">
-      <p>
-         Welcome to JesseVictors.com! Here you can view a bit about my background, check out what I know, and follow my blog for the latest news. If you're an employer and looking to hire me, you can find my latest resume <a href="files/resume.pdf">here</a>.
+   <div class="leftSide">
+      <div id="title">
+         JesseVictors.com
+      </div>
+      <p class="intro">
+         Welcome to JesseVictors.com! Here you can view a bit about my background, check out what I know, and follow my blog for the latest news. If you're an employer and looking to hire me, you can find my latest resume <a href="resources/resume.pdf">here</a>.
       </p>
-      <p>
-         While I have owned this website for close to a decade, over the last several years I have worked on expanding my presence online. A bit of Google-fu or clever searches on DuckDuckGo can find most of this, but for convience I'll list my main web presence here:
+      <p class="intro">
+         While I have owned this website for close to a decade, over the last several years I have worked on expanding my presence online. A bit of Google-fu or clever searches on DuckDuckGo can find most of this, but for convenience I'll list my main web presence here:
       </p>
       <p>
          <div class="title">Work</div>
-         <ul>
-            <li>Email
-               <div class="description">
-                  jvictors AT jessevictors DOT com. Send me a note!
-               </div>
-            </li>
-            <li>Github
-               <div class="description">
-                  <a href="https://github.com/Jesse-V">https://github.com/Jesse-V</a> Github is a popular website for organizing and managing open-source coding projects. I currently have 1000+ commits, 32 repositories, and 11 followers to my name. I use this site for most of my programming projects, which is a frequent activity. Some of my repositories are private, as I have a premium account.
-               </div>
-            </li>
-            <li>Tor
-               <div class="description">
-                  <a href="https://www.torproject.org/">Tor</a> is popular tool that protects Internet users from traffic analysis, surveillance, and government spying. I run all six of <a href="https://globe.torproject.org/#/search/query=utah">these relays</a>, two of which are exits, on my own servers. These machines also run <a href="http://usu.speedtest.net/">a Speedtest.net server</a>, a mirror of Linux Mint ISOs, and <a href="http://tuxnet-opti980.main.usu.edu/mirrors/torproject.org/">a mirror</a> of torproject.org.
-               </div>
-            </li>
-            <li>Launchpad
-               <div class="description">
-                  I package a few Linux packages, including Folding Atomata, my protein viewer for Folding@home, and <a href="https://github.com/Jesse-V/CurecoinSource#curecoin-client">CurecoinQT</a>, a graphical client for the Curecoin cryptocurrency. You can find my Launchpad profile <a href="https://launchpad.net/~jvictors">here</a>.
-               </div>
-            </li>
-            <li>Folding@home
-               <div class="description">
-                  <a href="http://folding.stanford.edu/home/">Folding@home</a> is a popular distributed computing project that simulates protein folding and other molecular dynamics for disease research. I wrote the majority of <a href="https://en.wikipedia.org/wiki/Folding@home">its Wikipedia article</a>, which now a Featured Article. I founded and led <a href="http://fah-web.stanford.edu/cgi-bin/main.py?qtype=teampage&teamnum=195965">this team</a> before I switched over to my <a href="http://fah-web.stanford.edu/cgi-bin/main.py?qtype=userpage&username=Jesse_V">most recent identity</a>.
-               </div>
-            </li>
-            <li>Folding Forum
-               <div class="description">
-                  <a href="https://foldingforum.org">The Folding Forum<a/> is the official support forum for the Folding@home project. I'm heavily involved in the forum and you can find my account <a href="https://foldingforum.org/memberlist.php?mode=viewprofile&u=18404">here.</a>
-            </li>
-            <li>IRC
-               <div class="description">
-                  I lurk and chat on several <a href="https://freenode.net/">Freenode IRC</a> channels, including #fah, #folding@home, and #curecoin. My username is @Jesse_V, and I'm opped on #fah and #curecoin. Log on and join me!
-               </div>
-            </li>
-         </ul>
-      <p>
+         <table>
+            <tr>
+               <td>Email<br>jvictors at jessevictors dot com</td>
+               <td>My main account. Send me a note!</td>
+            </tr>
+            <tr>
+               <td>Github - <a href="https://github.com/Jesse-V">Jesse-V</a></td>
+               <td>I currently have more than 1000 commits, 32 repositories, and 11 followers to my name. Some of my repositories are private, as I have a premium account.</td>
+            </tr>
+            <tr>
+               <td>Tor - <a href="https://globe.torproject.org/#/search/query=utah">relays/exits</a></td>
+               <td>I administer six high-speed Tor relays -- two of which are exits -- on my own servers. <a href="https://www.torproject.org/">Tor</a> helps protect millions of people from traffic analysis, surveillance, and government spying everyday, and I'm proud to do my part.</td>
+            </tr>
+            <tr>
+               <td>Launchpad - <a href="https://launchpad.net/~jvictors">~jvictors</a></td>
+               <td>I package a few Linux programs, mainly from my Github projects. I support Debian through .deb files, and *buntu/Mint through a PPA.</td>
+            </tr>
+            <tr>
+               <td>Folding@home</td>
+               <td>I wrote the majority of <a href="https://en.wikipedia.org/wiki/Folding@home">its Wikipedia article</a>, which now a Featured Article. I also founded and led <a href="http://fah-web.stanford.edu/cgi-bin/main.py?qtype=teampage&teamnum=195965">this team</a> before I switched over to my <a href="http://fah-web.stanford.edu/cgi-bin/main.py?qtype=userpage&username=Jesse_V">most recent identity</a>.</td>
+            </tr>
+            <tr>
+               <td>Folding Forum  - <a href="https://foldingforum.org/memberlist.php?mode=viewprofile&u=18404">Jesse_V</a></td>
+               <td>I'm heavily involved on the official Folding@home support forum and I help the Stanford researchers with many aspects of the F@h project.</td>
+            </tr>
+            <tr>
+               <td>Speedtest.net and Mirrors</td>
+               <td>My servers also host <a href="http://usu.speedtest.net/">a Speedtest.net server</a>, a <a href="http://tor-relay.cs.usu.edu/mirrors/linuxmint.com/">mirror of Linux Mint ISOs</a>, and a <a href="http://tuxnet-opti980.main.usu.edu/mirrors/torproject.org/">mirror of torproject.org</a>.</td>
+            </tr>
+            <tr>
+               <td>IRC</td>
+               <td>I lurk and chat on several <a href="https://freenode.net/">Freenode IRC</a> channels, including #fah, #folding@home, and #curecoin. My username is @Jesse_V, and I'm opped on #fah and #curecoin. Log on and join me!</td>
+            </tr>
+         </table>
+      </p>
       <p>
          <div class="title">Recreational</div>
-         <ul>
-            <li>Reddit
-               <div class="description">
-                  <a href="https://pay.reddit.com/">Reddit</a> is hilarious, fun, and educational all at the same time, I love it. I haven't once regretted leaving Facebook for this site. I'm <a href="https://pay.reddit.com/user/Jesse_V">/u/Jesse_V</a>, though I've been recently more active under some more anonymous accounts.
-               </div>
-            </li>
-            <li>Wikipedia
-               <div class="description">
-                  I'm <a href="https://en.wikipedia.org/wiki/User:Jesse_V.">User:Jesse_V.</a> I've been on the site since 2010, I'm an Online Ambassador, and I've made over 43,000 contributions to Wikipedia, making me one of the 1000 most active Wikipedians. I rewrote the entire <a href="https://en.wikipedia.org/wiki/Folding@home">Folding@home article</a> from scratch and brought it up to Featured Article status, and I approved <a href="https://en.wikipedia.org/wiki/Minecraft">Minecraft</a> as a Good Article.
-               </div>
-            </li>
-            <li>YouTube
-               <div class="description">
-                  I mostly subscribe to channels and watch gaming videos, but I have put out some timelapses. If you're interested, check out my channel at <a href="https://www.youtube.com/user/JesseVictors">/JesseVictors</a>.
-               </div>
-            </li>
-            <li>Twitter
-               <div class="description">
-                  I'm not very active here, but you can still follow me at <a href="https://twitter.com/Jesse_Victors">@Jesse_Victors</a> if you like.
-               </div>
-            </li>
-         </ul>
+         <table>
+            <tr>
+               <td>Reddit - <a href="https://pay.reddit.com/user/Jesse_V">/u/Jesse_V</a></td>
+               <td>I love this site and I visit it frequently, athough lately I've been more active under my more anonymous account.</td>
+            </tr>
+            <tr>
+               <td>Wikipedia - <a href="https://en.wikipedia.org/wiki/User:Jesse_V.">User:Jesse_V</a></td>
+               <td>I've been on the site since 2010, I'm an Online Ambassador, and I've made over 43,000 contributions to Wikipedia. I rewrote the entire <a href="https://en.wikipedia.org/wiki/Folding@home">Folding@home article</a> from scratch and brought it up to Featured Article status, and I approved <a href="https://en.wikipedia.org/wiki/Minecraft">Minecraft</a> as a Good Article.</td>
+            </tr>
+            <tr>
+               <td>YouTube - <a href="https://www.youtube.com/user/JesseVictors">JesseVictors</a></td>
+               <td>I mostly subscribe to channels and watch gaming videos, but I have put out some timelapses. If you're interested, check out my channel!</td>
+            </tr>
+            <tr>
+               <td>Twitter - <a href="https://twitter.com/Jesse_Victors">@Jesse_Victors</a></td>
+               <td>I'm not very active here, but you can still follow me if you like.</td>
+            </tr>
+         </table>
       </p>
       <p>
          <div class="title">Cryptocurrencies</div>
-         <ul>
-            <li>TipChange
-               <div class="description">
-                  Impress me on Github, Reddit, or Twitter and I might end up using <a href="https://www.changetip.com">changetip.com</a> to tip you some coins! I'm <a href="https://www.changetip.com/tipsters/Jesse-V">tipster Jesse-V</a>.
-               </div>
-            </li>
-            <li>Curecoin Address (volume: ) and username</li>
-            <li>Bitcoin Address (volume: )</li>
-            <li>Dogecoin Address (volume )</li>
-         </ul>
+         <table>
+            <tr>
+               <td>TipChange - <a href="https://www.changetip.com/tipsters/Jesse-V">Tipster Jesse-V</a></td>
+               <td>Impress me on Github, Reddit, or Twitter and I might end up tipping you some coins!</td>
+            </tr>
+            <tr>
+               <td>Bitcoin</td>
+               <td><img src="images/BTC_addr.png" alt="Bitcoin address QR"></td>
+            </tr>
+            <tr>
+               <td>Curecoin</td>
+               <td>Combines the Folding@home project with Bitcoin technology. BJEWt96yDpAo8HFwnmijfvb1JkT8axSoe1 is my public wallet.</td>
+            </tr>
+            <tr>
+               <td>Dogecoin</td>
+               <td>To the moon! Tip DNSsCF34UBgcuvY23CdgnWoC2GoZGggVLo you adventurous shibes!</td>
+            </tr>
+         </table>
       </p>
    </div>
 
